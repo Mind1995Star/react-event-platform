47a5c15231966a516fe3a6eeac06fd4b55cc21e6
Mind1995Star <105137625+Mind1995Star@users.noreply.github.com>
2022-08-13 12:15:47 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ApolloProvider } from '@apollo/client'
+import { client } from './lib/apollo'
+import { BrowserRouter } from 'react-router-dom'
+import Router from "./Router"
+
+export default function App() {
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  )
+}
---
+++ src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+// IMPORT CSS
+import './styles/global.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+)
+
+// TypeScript + JSX (XML dentro do Javascript)
+
+// TypeScript + JSX (XML dentro do Javascript)
+
+// CMS - Content Management System
+// Wordpress não é Headless, pois entrega tanto o painel de ADMIN quanto o Frontend (temas)
+// Headless CMS (GraphCMS): Painel de ADMIN (Dados fornecidos através de uma API REST ou GraphQL)
+// React consome essa API do CMS
